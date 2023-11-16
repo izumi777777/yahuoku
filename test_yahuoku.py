7ab07dc1fd25f855a88fc476a2f6980a25af5e70
kikumura01 <kikumura804@gmail.com>
2023-11-17 00:49:09 +0900

    add

--- test_yahuoku.py
+++ test_yahuoku.py
@@ -7,6 +7,8 @@ from bs4 import BeautifulSoup  # Webサイト情報の取得用
 # ========================================================
 # 静的解析対象Webサイトの初期URLを宣言
 # ========================================================
+# 取得したいヤフオクの落札相場のURLを入力する
+# base_url = input("落札相場を取得したヤフオクのURLを入力してください: ")
 base_url = "https://auctions.yahoo.co.jp/closedsearch/closedsearch?va=ONKYO+FR-N7&b=1&n=100&auccat=&tab_ex=commerce&ei=utf-8&aq=-1&oq=&sc_i=&exflg=&p=ONKYO+FR-&x=0&y=0"
 print("1回目のURL : " + base_url)
 
